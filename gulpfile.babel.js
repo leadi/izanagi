52c944c0ed87be68f0e36511c86662f7166d29d1
masa69 <psychedelic.nekopunch@gmail.com>
2016-09-08 20:23:35 +0900

    gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,31 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('sass', () => {
+	gulp.src('./demo/sass/style.scss')
+		.pipe($.plumber())
+		// .pipe($.sourcemaps.init())
+		.pipe($.sass({
+			// includePaths: ['.']
+			// includePaths : [require("bourbon").includePaths],
+			// [outputStyle] Type: String Default: nested Values: nested, expanded, compact, compressed
+			outputStyle  : 'nested',
+		}).on('error', $.sass.logError))
+		.pipe($.autoprefixer())
+		// .pipe($.sourcemaps.write('./'))
+		.pipe(gulp.dest('./demo/css'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch([
+		'*.scss',
+		'assets/*.scss',
+		'assets/**/*.scss'
+	],['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build']);
